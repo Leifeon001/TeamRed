58aff5b488e13912936fa7d634bee8d5b977cb6f
Rimour <63150407+Rimour@users.noreply.github.com>
2020-06-06 06:03:55 -0400

    basic main menu function scene added

---
+++ teamRedMech/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject OptPanel;
+    bool isOptOpen;
+    public GameObject CredPanel;
+    bool isCredOpen;
+
+    public void Options() //click Options panel on/off
+    {
+       if(isOptOpen == false)
+        {
+
+            isOptOpen = true;
+            OptPanel.SetActive(true);
+            isCredOpen = false;
+            CredPanel.SetActive(false);
+
+        }
+        else if (isOptOpen == true)
+        {
+            isOptOpen = false;
+            OptPanel.SetActive(false);
+        }
+        
+    }
+
+    public void Credits() //click Options panel on/off
+    {
+        if(isCredOpen == false)
+        {
+            isCredOpen = true;
+            CredPanel.SetActive(true);
+            isOptOpen = false;
+            OptPanel.SetActive(false);
+        }
+        else if (isCredOpen == true)
+        {
+            isCredOpen = false;
+            CredPanel.SetActive(false);
+
+        }
+    }
+    
+    public void PlayGame() //Start Playing game
+    {
+
+    }
+    public void QuitGame() //Exit game
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
