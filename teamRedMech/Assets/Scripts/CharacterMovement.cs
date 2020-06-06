b0902c863b480d7190ceaee909a6b06754272ecb
Leifeon001 <tristen.howk@yahoo.com>
2020-06-06 18:24:39 -0400

    Updated Player Movement
    Fixed Player Movement to add Rotation

--- teamRedMech/Assets/Scripts/CharacterMovement.cs
+++ teamRedMech/Assets/Scripts/CharacterMovement.cs
@@ -9,23 +9,34 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 1f;
     public float forward;
     public float sideways;
-    
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        CharacterMove(); 
+        if (Input.GetKeyDown(KeyCode.Space) && Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && Cursor.lockState != CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        CharacterMove();
     }
 
     void CharacterMove()
     {
-        //Vector3 mousePosition = Input.mousePosition;
+        //transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+   
         //mousePosition.y = 0;
         forward = Input.GetAxis("Vertical");
         sideways = Input.GetAxis("Horizontal");
-        Vector3 characterMovement = new Vector3(sideways, 0, forward);
+        Vector3 characterMovement = new Vector3(0, 0, forward);
+        characterMovement = transform.TransformDirection(characterMovement);
         characterMovement *= speed;
         move.Move(characterMovement * Time.deltaTime);
-       // player.LookAt(mousePosition);
+        transform.Rotate(0, sideways, 0);
+        // player.LookAt(mousePosition);
     }
 }
