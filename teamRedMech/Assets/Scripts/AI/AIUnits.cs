366d18b87a3e882dd05ce357f4486f91f00f72ce
Leifeon001 <tristen.howk@yahoo.com>
2020-06-06 06:15:21 -0400

    Adding AI Units
    Added AI Functionallity and AI damage code

---
+++ teamRedMech/Assets/Scripts/AI/AIUnits.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIUnits : MonoBehaviour
+{
+    #region Stats
+    [Header("Stats")]
+    public float health;
+    public float damage;
+    public float LookRadius;
+    public float Atkzone;
+    public float Speed;
+    #endregion
+    [HideInInspector] public bool Ally = false;
+    [HideInInspector] public NavMeshAgent agent;
+    public float wander;
+    public List<GameObject> Enemies;
+    Vector3 randPoint = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+ 
+    }
+    public void EMove()
+    {
+        randPoint.x = Random.Range(-wander, wander);
+        randPoint.y = Random.Range(-wander, wander);
+        randPoint.z = Random.Range(-wander, wander);
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(transform.position, out hit, wander, NavMesh.AllAreas))
+        {
+            agent.SetDestination(randPoint);
+        }
+        //Vector3 temp = Vector3.forward * Speed;
+        //transform.position += temp;
+        ////transform.position = new vector3(random.insideunitsphere.x * lookradius , transform.position.y, random.insideunitsphere.z * lookradius);
+    }
+    public void FaceTarget(Transform Hunt)
+    {
+        Vector3 direction = (Hunt.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 7);
+        agent.SetDestination(Hunt.position);
+    }
+    public void CurrHealth(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Debug.Log("Unit " + gameObject + " has been killed");
+        }
+    }
+    public virtual void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
+    }
+}
---
+++ teamRedMech/Assets/Scripts/AI/EnemyUnit.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : AIUnits
+{
+    public override void Update()
+    {
+        base.Update();
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, LookRadius);
+        //Debug.Log("Hello, I am Enemy AI");
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            //Debug.Log(hitColliders[i].name);
+            CharacterMovement Character = hitColliders[i].GetComponent<CharacterMovement>();
+            if (Character != null && !Enemies.Contains(hitColliders[i].gameObject))
+            {
+                Enemies.Add(hitColliders[i].gameObject);
+                if (Enemies != null)
+                {
+                    //Debug.Log("Enemy found!");
+                    agent.SetDestination(Enemies[0].transform.position);
+                }
+            }
+            else if (Enemies.Contains(hitColliders[i].gameObject))
+            {
+                if (Enemies[0] != null)
+                {
+                    float distance = Vector3.Distance(Enemies[0].transform.position, transform.position);
+                    Transform Chase = Enemies[0].transform;
+                    if (distance <= agent.stoppingDistance)
+                    {
+                        //Attack Target 
+                        FaceTarget(Chase);
+                    }
+                }
+            }
+            else if (Enemies.Count == 0)
+            {
+                if (agent.remainingDistance == 0)
+                {
+                    EMove();
+                }
+            }
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i] != null)
+            {
+                float distance = Vector3.Distance(Enemies[0].transform.position, transform.position);
+                if (distance <= Atkzone)
+                {
+                    agent.SetDestination(Enemies[0].transform.position);
+                }
+                else
+                {
+                    Enemies.Clear();
+                }
+            }
+            
+        }
+    }
+}
