4e3c64093bf14589e8f3917944328f668916c83b
Rimour <63150407+Rimour@users.noreply.github.com>
2020-06-06 08:31:18 -0400

    added graphic function

---
+++ teamRedMech/Assets/Scripts/SetGraphics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetGraphics : MonoBehaviour
+{
+  public void Graphics(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void FullScreen (bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+
+    public void WindowScreen(bool isWindow)
+    {
+
+    }
+}
