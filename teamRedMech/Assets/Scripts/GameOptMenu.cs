c8439ff1b9439225073576b56dd42956a6fbe0e0
Rimour <63150407+Rimour@users.noreply.github.com>
2020-06-07 14:49:14 -0400

    Added Options menu

---
+++ teamRedMech/Assets/Scripts/GameOptMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOptMenu : MonoBehaviour
+{
+    public GameObject OptPanel;
+    bool OptOpen;
+
+    
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && OptOpen == false)
+        {
+            OptOpen = true;
+            OptPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }else if (Input.GetKeyDown(KeyCode.Escape)&& OptOpen == true)
+        {
+            OptOpen = false;
+            OptPanel.SetActive(false);
+            Time.timeScale = 1f;
+
+        }
+    }
+}
