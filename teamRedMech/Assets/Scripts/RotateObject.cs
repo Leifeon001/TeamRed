705f1ceba292d692db9adf8d42fda68c950a3fe3
Rimour <63150407+Rimour@users.noreply.github.com>
2020-06-06 20:36:02 -0400

    updated Menu with Drop Pod in back

---
+++ teamRedMech/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float Rotspeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Rotspeed * Time.deltaTime);
+    }
+}
